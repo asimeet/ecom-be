460a6a3e66d2cdda107d937eeb86835b9b4c8044
Asimeet Padhee <asimeet@asimeets-mbp.kpn>
2021-09-09 01:03:05 +0200

    beta-003

--- src/mock-data/ugc.js
+++ src/mock-data/ugc.js
@@ -2,43 +2,43 @@ module.exports = {
     imageUrls: [
         {       
           productId: '12345',
-          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/15f901c90a9549d29104aae700d27efb_9366/Superstar_Schoenen_Zwart_EG4959_01_standard.jpg',
-          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/15f901c90a9549d29104aae700d27efb_9366/Superstar_Schoenen_Zwart_EG4959_01_standard.jpg',
+          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/e2e54edc0b1c4baa9489ac0e00bcf89c_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_01_standard.jpg',
+          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/e2e54edc0b1c4baa9489ac0e00bcf89c_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_01_standard.jpg',
         },
         {       
           productId: '12345',
-          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/4785b631bbf64d648245aae601100c28_9366/Superstar_Schoenen_Zwart_EG4959_010_hover_standard.jpg',
-          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/4785b631bbf64d648245aae601100c28_9366/Superstar_Schoenen_Zwart_EG4959_010_hover_standard.jpg',
+          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/e426ee27ee884e65b8b2ac0e00bd06ca_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_02_standard_hover.jpg',
+          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/e426ee27ee884e65b8b2ac0e00bd06ca_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_02_standard_hover.jpg',
         },
         {       
           productId: '12345',
-          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/ff0654f4089f4413baa7aae700d2a08c_9366/Superstar_Schoenen_Zwart_EG4959_02_standard.jpg',
-          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/ff0654f4089f4413baa7aae700d2a08c_9366/Superstar_Schoenen_Zwart_EG4959_02_standard.jpg',
+          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/125a2cb28d544baebf01ac0e00bd0da6_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_03_standard.jpg',
+          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/125a2cb28d544baebf01ac0e00bd0da6_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_03_standard.jpg',
         },
         {       
           productId: '12345',
-          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/9aa935080d1849a1829daae700d2aba1_9366/Superstar_Schoenen_Zwart_EG4959_03_standard.jpg',
-          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/9aa935080d1849a1829daae700d2aba1_9366/Superstar_Schoenen_Zwart_EG4959_03_standard.jpg'
+          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/300b4652088e4968bd5fac0e00cab975_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_04_standard.jpg',
+          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/300b4652088e4968bd5fac0e00cab975_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_04_standard.jpg'
         },
         {       
           productId: '12345',
-          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/7c365af056f34e6bacf4aae700d2b7fe_9366/Superstar_Schoenen_Zwart_EG4959_04_standard.jpg',
-          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/7c365af056f34e6bacf4aae700d2b7fe_9366/Superstar_Schoenen_Zwart_EG4959_04_standard.jpg'
+          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/6ad2c8db0ae041db9acbac0e00cac13e_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_05_standard.jpg',
+          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/6ad2c8db0ae041db9acbac0e00cac13e_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_05_standard.jpg'
         },
         {       
           productId: '12345',
-          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/ebe5146ef13d4d9096a5aae700d2c437_9366/Superstar_Schoenen_Zwart_EG4959_05_standard.jpg',
-          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/ebe5146ef13d4d9096a5aae700d2c437_9366/Superstar_Schoenen_Zwart_EG4959_05_standard.jpg'
+          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/dfe7a2e4a5f04849946fac0e00bcfff8_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_06_standard.jpg',
+          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/dfe7a2e4a5f04849946fac0e00bcfff8_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_06_standard.jpg'
         },
         {       
           productId: '12345',
-          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/6f0f4c6d9fa14c1b80e6aae700d350d8_9366/Superstar_Schoenen_Zwart_EG4959_09_standard.jpg',
-          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/6f0f4c6d9fa14c1b80e6aae700d350d8_9366/Superstar_Schoenen_Zwart_EG4959_09_standard.jpg'
+          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/ea4064c2839d4d7fa804ac0e00cac862_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_41_detail.jpg',
+          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/ea4064c2839d4d7fa804ac0e00cac862_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_41_detail.jpg'
         },
         {       
           productId: '12345',
-          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/37cbe7a6558446849011aae700d2d002_9366/Superstar_Schoenen_Zwart_EG4959_41_detail.jpg',
-          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/37cbe7a6558446849011aae700d2d002_9366/Superstar_Schoenen_Zwart_EG4959_41_detail.jpg'
+          original: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/4fe0627c19a84c86ae62ac0e00cacf49_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_42_detail.jpg',
+          thumbnail: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/4fe0627c19a84c86ae62ac0e00cacf49_9366/Court_Team_Bounce_Indoor_Schoenen_Zwart_FZ2615_42_detail.jpg'
         }
       ]
 }
